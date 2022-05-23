1c4875b1b8eba9e45c28177154a41073e709b035
khotcholava <akokhocholava@gmail.com>
2022-05-24 03:02:21 +0400

    add several examples

--- index.ts
+++ index.ts
@@ -23,6 +23,7 @@ type Item = {
     b: number;
     c: boolean;
 }
+const foo = (name: string, age: number) =>'Hello'
 
 type T1 = {
     // ! for let P in ['x', 'y']
@@ -42,12 +43,66 @@ type T3 = {
 
 // ! Pick type
 
-type MyPick<T, K extends keyof T> = {[P in K]: T[P]}
+type MyPick<T, K extends keyof T> = { [P in K]: T[P] }
 
 type PickType = MyPick<Item, 'a' | 'b'>
 
 // ! Readonly type
 
-type MyReadonly<T> = {readonly [P in keyof T]: T[P]}
+type MyReadonly<T> = { readonly [P in keyof T]: T[P] }
 
 type ReadonlyType = MyReadonly<Item>
+
+
+// ! Tuple to object
+
+type TupleToObject<T extends readonly any[]> = {
+    [P in T[number]]: P
+}
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+const arr = ['tesla', 'model 3', 'model X', 'model Y'];
+type MyTuple = TupleToObject<typeof tuple>
+
+// ! First of array
+
+type FirstOfArray<T extends any[]> = T[number] extends never ? never : T[0]
+type MyFirstOfArray = FirstOfArray<typeof arr>
+
+// ! Length of Tuple
+/*
+    ? The point in this problem is that you need to use readonly. As you can see
+    ? from as const in the case data, the passed value is a tuple type, which you cannot make any change on it.
+    ? Since it is a tuple type, you need to add readonly to guarantee that the length of the array will not change.
+*/
+
+type TupleLength<T extends readonly any[]> = T['length']
+type MyTupleLength = TupleLength<typeof tuple>
+
+// ! If
+
+type If<C, T, F> = C extends true ? T : F;
+type MyIf = If<false, 1, 2>
+
+// Concat
+
+type Concat<T extends any[], U extends any[]> = [...T, ...U]
+type MyConcat = Concat<[1, 2, 3], ['a', 'b', 'c']>
+
+// ! Push
+
+type Push<T extends any[], U> = [...T, U]
+type MyPush = Push<[1, 2], 3>
+
+// ! Exclude
+type MyExclude<T, U> = T extends U ? never : T
+type MyExcludeT = MyExclude<'a' | 'b' | 'c', 'a'>
+
+// ! Includes
+type MyIncludes<T extends any[], U> = U extends T[number] ? true : false
+type MyIncludesT = MyIncludes<[boolean, 2, 3, 5, 6, 7], false>
+
+// ! Parameters
+
+type MyParameters<T> = T extends (...args: infer F) => any ? F: never
+type MyParametersT = MyParameters<typeof foo>
